4e2e341699962b7abab2af3620590edfd163349a
Zh31n <zh31n@mail.com>
2023-01-25 19:00:36 +0300

    метод массива map и создание типо базы данных

--- src/components/Messanger/Messanger.jsx
+++ src/components/Messanger/Messanger.jsx
@@ -3,46 +3,57 @@ import s from './Messanger.module.css'
 import UserName from './UserName/UserName';
 
 
+
+
 let Messanger = () => {
+
+    let UserData = [
+        { UserName: 'Leha', id: 1 },
+        { UserName: 'Sanya', id: 2 },
+        { UserName: 'Nebrik', id: 3 },
+        { UserName: 'Dima', id: 4 },
+        { UserName: 'Maksim', id: 5 },
+        { UserName: 'Artur', id: 6 },
+        { UserName: 'Leha', id: 7 },
+        { UserName: 'Sanya', id: 8 },
+        { UserName: 'Nebrik', id: 9 },
+        { UserName: 'Dima', id: 10 },
+        { UserName: 'Maksim', id: 11 },
+        { UserName: 'Artur', id: 12 },
+        { UserName: 'Leha', id: 13 },
+        { UserName: 'Sanya', id: 14 },
+        { UserName: 'Nebrik', id: 15 },
+        { UserName: 'Dima', id: 16 },
+        { UserName: 'Maksim', id: 17 },
+        { UserName: 'Artur', id: 18 },
+        { UserName: 'Leha', id: 19 },
+        { UserName: 'Sanya', id: 20 },
+        { UserName: 'Nebrik', id: 21 },
+        { UserName: 'Dima', id: 22 },
+        { UserName: 'Maksim', id: 23 },
+        { UserName: 'Artur', id: 24 },
+        { UserName: 'Leha', id: 25 },
+        { UserName: 'Sanya', id: 26 },
+        { UserName: 'Nebrik', id: 27 },
+        { UserName: 'Dima', id: 28 },
+        { UserName: 'Maksim', id: 29 },
+        { UserName: 'Artur', id: 30 }
+    ];
+
+    let userElements = UserData.map((user) => {return <UserName UserName={user.UserName} id={user.id} />})
+
+    let messageData = [
+        { message: 'hey', id: 1 },
+        { message: 'На цирлах', id: 2 },
+        { message: 'Иди своей дорогой сталкер', id: 3 }
+    ];
+
+    let messageElement = messageData.map((message) => {return <MessageItem message={message.message} />})
+
     return (
         <div className={s.content}>
-            <div className={s.names}>
-                <UserName UserName='Leha' id='1'/>
-                <UserName UserName='Sanya' id='2'/>
-                <UserName UserName='Nebrik' id='3'/>
-                <UserName UserName='Dima' id='4'/>
-                <UserName UserName='Maksim' id='5'/>
-                <UserName UserName='Artur' id='6'/>
-                <UserName UserName='Leha' id='7'/>
-                <UserName UserName='Sanya' id='8'/>
-                <UserName UserName='Nebrik' id='9'/>
-                <UserName UserName='Dima' id='10'/>
-                <UserName UserName='Maksim' id='11'/>
-                <UserName UserName='Artur' id='12'/>
-                <UserName UserName='Leha' id='13'/>
-                <UserName UserName='Sanya' id='14'/>
-                <UserName UserName='Nebrik' id='15'/>
-                <UserName UserName='Dima' id='16'/>
-                <UserName UserName='Maksim' id='17'/>
-                <UserName UserName='Artur' id='18'/>
-                <UserName UserName='Leha' id='19'/>
-                <UserName UserName='Sanya' id='20'/>
-                <UserName UserName='Nebrik' id='21'/>
-                <UserName UserName='Dima' id='22'/>
-                <UserName UserName='Maksim' id='23'/>
-                <UserName UserName='Artur'id='24'/>
-                <UserName UserName='Leha' id='25'/>
-                <UserName UserName='Sanya'id='26'/>
-                <UserName UserName='Nebrik' id='27'/>
-                <UserName UserName='Dima' id='28'/>
-                <UserName UserName='Maksim' id='29'/>
-                <UserName UserName='Artur' id='30'/>
-            </div>
-            <div className={s.messages}>
-                <MessageItem message='hey'/>
-                <MessageItem message='На цирлах'/>
-                <MessageItem message='Иди своей дорогой сталкер'/>
-            </div>
+            <div className={s.names}>{userElements}</div>
+            <div className={s.messages}>{messageElement}</div>
         </div>
     );
 }
--- src/components/Profile/ProfilePosts/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts/ProfilePosts.jsx
@@ -2,6 +2,15 @@ import PostItem from './PostItem/PostItem';
 import s from './ProfilePosts.module.css'
 
 let ProfilePosts = () => {
+
+    let postData = [
+        { postTxt: 'Здарова епта', likesCount: ' 0' },
+        { postTxt: 'Я фронтом скоро стану', likesCount: ' 5' },
+        { postTxt: 'Иди своей дорогой сталкер', likesCount: ' 10' }
+    ];
+
+    let postElements = postData.map( post =>  <PostItem message={post.postTxt} likes={post.likesCount} />)
+
     return (
         <div className={s.prof_posts}>
             My posts
@@ -10,13 +19,10 @@ let ProfilePosts = () => {
                 <button>add post</button>
             </div>
             <div className={s.posts}>
-                <PostItem message='Здарова епта' likes=' 0'/>
-                <PostItem message='Я фронтом скоро стану' likes=' 5'/>
-                <PostItem message='Иди своей дорогой сталкер' likes=' 10'/>
-                {/* <PostItem />
-                <PostItem />
-                <PostItem />
-                <PostItem /> */}
+                {/* <PostItem message={postData[0].postTxt} likes={postData[0].likesCount} />
+                <PostItem message={postData[1].postTxt} likes={postData[1].likesCount} />
+                <PostItem message={postData[2].postTxt} likes={postData[2].likesCount} /> */}
+                {postElements}
             </div>
         </div>
     );
