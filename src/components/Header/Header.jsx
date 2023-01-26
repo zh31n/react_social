100c63b0ed2678e3dc6a4465e301995c5c1e2659
Zh31n <zh31n@mail.com>
2023-01-26 14:46:45 +0300

    берем значение из инпута и при клике видим что ввели

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
-import s from'./Header.module.css';
+import s from './Header.module.css';
 
 
-let Header = () =>{
-    return(
+let Header = () => {
+    return (
         <div className={s.header}>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Vanamo_Logo.svg/800px-Vanamo_Logo.svg.png" alt="" />
+            <div className={s.container}>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Vanamo_Logo.svg/800px-Vanamo_Logo.svg.png" alt="" />
+            </div>
         </div>
     );
 }
--- src/components/Profile/ProfilePosts/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts/ProfilePosts.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import PostItem from './PostItem/PostItem';
 import s from './ProfilePosts.module.css'
 
@@ -5,12 +6,19 @@ let ProfilePosts = (props) => {
     
     let postElements = props.state.posts.map(post => <PostItem message={post.postTxt} likes={post.likesCount} />)
 
+    let inputValue = React.createRef();
+
+    let addPost = () => {
+        let text = inputValue.current.value;
+        alert(text)
+    }
+
     return (
         <div className={s.prof_posts}>
             My posts
             <div className={s.new_posts}>
-                <textarea name="" id=""></textarea>
-                <button>add post</button>
+                <textarea ref={inputValue} name="" id=""></textarea>
+                <button onClick={addPost}>add post</button>
             </div>
             <div className={s.posts}>
                 {postElements}
