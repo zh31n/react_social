458a434beaf6b3d25b672b86e9abc8e97bf09b1b
Zh31n <zh31n@mail.com>
2023-02-10 10:35:29 +0300

    page find users,use api for this

--- src/Redux/UsersReducer.js
+++ src/Redux/UsersReducer.js
@@ -1,11 +1,16 @@
 const UNFOLLOW = 'UNFOLLOW';
 const FOLLOW = 'FOLLOW';
 const SET_USERS = 'SET_USERS';
+const SET_PAGE = 'SET_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
 let randomId = Math.random();
 
 let initialDefault = {
-    users: []
+    users: [],
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage: 1
 };
 
 const UsersReducer = (state = initialDefault, action) => {
@@ -32,8 +37,17 @@ const UsersReducer = (state = initialDefault, action) => {
             };
         }
         case SET_USERS: {
-            return { ...state, users: [...state.users, ...action.users] }
+            return { ...state, users: action.users }
         }
+
+        case SET_PAGE: {
+            return { ...state, currentPage: action.currentPage }
+        }
+
+        case SET_TOTAL_USERS_COUNT: {
+            return { ...state, totalUsersCount: action.count }
+        }
+
         default:
             return state;
     }
@@ -44,5 +58,13 @@ export const followAC = (userId) => { return { type: FOLLOW, userId } }
 export const unFollowAC = (userId) => { return { type: UNFOLLOW, userId } }
 
 export const setUsersAC = (users) => { return { type: SET_USERS, users } }
+
+export const setCurrentPageAC = (currentPage) => { return { type: SET_PAGE, currentPage } }
+
+export const setTotalUsersCountAC = (totalUsersCount) => { return { type: SET_TOTAL_USERS_COUNT, count: totalUsersCount } }
+
+
+
+
 
 export default UsersReducer;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,24 +1,33 @@
 import { connect } from 'react-redux';
-import { followAC, setUsersAC, unFollowAC } from '../../Redux/UsersReducer';
+import { followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unFollowAC } from '../../Redux/UsersReducer';
 import Users from './Users';
 
 let mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage
     }
 
 }
 
 let mapDispatchToProps = (dispatch) => {
     return {
-        follow : (userId) => {
+        follow: (userId) => {
             dispatch(followAC(userId))
         },
-        unFollow : (userId) => {
+        unFollow: (userId) => {
             dispatch(unFollowAC(userId))
         },
-        setUsers : (users) => {
+        setUsers: (users) => {
             dispatch(setUsersAC(users))
+        },
+        setCurrentPage: (p) => {
+            dispatch(setCurrentPageAC(p))
+        },
+        setTotalUsersCount: (totalUsersCount) => {
+            dispatch(setTotalUsersCountAC(totalUsersCount))
         }
     }
 }
